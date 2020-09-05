9dd635cb0f565b4b39358e260be68e1c561d7685
Your Nameart555w <art555m@gmail.com>
2020-09-05 22:25:30 +0500

    added section items

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,58 @@ $('.header__burger').on('click', function () {
 $('.slider__body').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
+	asNavFor: '.slider__nav',
 	arrows: false,
-	asNavFor: '.slider__nav'
 });
+
 $('.slider__nav').slick({
 	slidesToShow: 6,
 	asNavFor: '.slider__body',
-	focusOnSelect: true
+	focusOnSelect: true,
+	dots: false,
+	arrows: false,
+	responsive: [
+		{
+			breakpoint: 960,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				focusOnSelect: true
+			}
+		},
+		{
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				focusOnSelect: true
+			}
+		},
+		{
+			breakpoint: 670,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				focusOnSelect: true
+			}
+		},
+		{
+			breakpoint: 500,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				focusOnSelect: true
+			}
+		},
+		{
+			breakpoint: 350,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				focusOnSelect: true
+			}
+		},
+	]
 });
+
+
